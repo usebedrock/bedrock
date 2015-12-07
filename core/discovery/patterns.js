9ff4be7fa627aa345c132313b9a85a86c5fa5829
Thomas Tuts <thomas@thomastuts.com>
2015-12-07 17:48:10 +0100

    Fix pattern discovery port

--- core/discovery/patterns.js
+++ core/discovery/patterns.js
@@ -1,21 +1,3 @@
-var glob = require('glob');
-var path = require('path');
-var _ = require('lodash');
-var config = require('../config');
-var paths = require('../paths');
-
-var TEMPLATES_BASE_DIRECTORY = paths.content.templates.patterns;
-
-function discover() {
-  var files = glob.sync(path.join(TEMPLATES_BASE_DIRECTORY, '**/*.jade'));
-  var filemap = {
-    base: [],
-    categories: []
-  };
-
-  files.forEach(function (file) {
-    var filename = file.replace(TEMPLATES_BASE_DIRECTORY, '').replace('.jade', '');
-    var parts = filename.split('/');
 'use strict';
 
 const glob = require('glob');
@@ -86,62 +68,6 @@ function discover() {
     byGroup: patternGroups,
     byCategory: _.groupBy(patternGroups, (pattern) => pattern.category.humanized || 'No category')
   };
-}
-
-module.exports = {
-  discover: discover
-};
-    var categoryName, patternName;
-
-    if (parts.length === 1) {
-      patternName = parts[0];
-      filemap.base.push({
-        filename,
-        name: patternName,
-        url: '/styleguide#' + patternName
-      });
-    }
-    else {
-      categoryName = parts[0];
-      patternName = parts[1];
-      var category = _.find(filemap.categories, {'name': categoryName});
-
-      if (!category) {
-        category = {
-          name: categoryName,
-          patterns: []
-        };
-
-        filemap.categories.push(category);
-      }
-
-      var patternData = {
-        filename,
-        name: patternName,
-        url: '/styleguide#' + patternName,
-        extraClasses: []
-      };
-
-      if (config.patternClasses) {
-        var patternCategoryClass = config.patternClasses[categoryName];
-        var patternSpecificClass = config.patternClasses[categoryName + '.' + patternName];
-
-        if (patternCategoryClass) {
-          patternData.extraClasses.push(patternCategoryClass);
-        }
-
-        if (patternSpecificClass) {
-          patternData.extraClasses.push(patternSpecificClass);
-        }
-
-        patternData.extraClasses = patternData.extraClasses.join(' ');
-      }
-
-      category.patterns.push(patternData);
-    }
-  });
-
-  return filemap;
 }
 
 module.exports = {
