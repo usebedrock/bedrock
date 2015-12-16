23f5e59aa381cb5e26355965b6ce5466a427e359
Thomas Tuts <thomas@thomastuts.com>
2015-12-16 12:25:08 +0100

    Add collapsible modules in prototype nav and remember state (fixes #37)

--- core/js/prototype-nav.js
+++ core/js/prototype-nav.js
@@ -3,13 +3,17 @@ const $ = require('jquery');
 const ACTIVATION_KEYCODE = 77; // 'M' key
 const ESC_KEYCODE = 27;
 const NAV_STATE_STORAGE_KEY = 'bedrock.prototypeNavState';
+const MODULE_LABEL_CLASS = 'br-bordered-list-label';
+const MODULE_IS_COLLAPSED_CLASS = 'br-prototype-nav-item-is-collapsed';
+
+let navState = {
+  isOpen: false,
+  closedModules: []
+};
 
 const $html = $('html');
 const $prototypeNav = $('#__prototype-nav');
-
-let navState = {
-  isOpen: false
-};
+const $moduleLabels = $prototypeNav.find(`.${MODULE_LABEL_CLASS}`);
 
 try {
   const savedState = JSON.parse(localStorage.getItem(NAV_STATE_STORAGE_KEY));
@@ -17,8 +21,28 @@ try {
 } catch (err) {
 }
 
+function addModuleToClosedModules(module) {
+  navState.closedModules.push(module);
+  saveNavState();
+}
+
+function removeModuleFromClosedModules(module) {
+  navState.closedModules = navState.closedModules.filter(m => m !== module);
+  saveNavState();
+}
+
+// Handle state on page load
 if (navState.isOpen) {
   open();
+}
+
+for (const moduleId of navState.closedModules) {
+  const $moduleParent = $prototypeNav
+    .find(`.${MODULE_LABEL_CLASS}[data-module="${moduleId}"]`)
+    .parents('li')
+    .eq(0);
+
+  $moduleParent.addClass(MODULE_IS_COLLAPSED_CLASS);
 }
 
 function saveNavState() {
@@ -42,6 +66,19 @@ function close() {
 function toggle() {
   navState.isOpen ? close() : open();
 }
+
+$moduleLabels.on('click', function () {
+  const moduleId = $(this).data('module');
+  const $parent = $(this).parents('li').eq(0);
+
+  if ($parent.hasClass(MODULE_IS_COLLAPSED_CLASS)) {
+    $parent.removeClass(MODULE_IS_COLLAPSED_CLASS);
+    removeModuleFromClosedModules(moduleId);
+  } else {
+    $parent.addClass(MODULE_IS_COLLAPSED_CLASS);
+    addModuleToClosedModules(moduleId);
+  }
+});
 
 $(window).on('keyup', function (e) {
   if (e.keyCode === ESC_KEYCODE) {
