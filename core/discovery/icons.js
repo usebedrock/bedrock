900f9d2a43f72d622ce96a5d30d9bfca46cc335d
Thomas Tuts <thomas@thomastuts.com>
2017-05-31 18:31:52 +0200

    Add support for adding categories to icons (#152)

--- core/discovery/icons.js
+++ core/discovery/icons.js
@@ -1,22 +1,32 @@
-'use strict';
-
 const path = require('path');
 const glob = require('glob');
+const groupBy = require('lodash/collection/groupBy');
+
 const paths = require('../paths');
 
-const ICONS_DIRECTORY = paths.content.icons.sourceDirectory;
+function isSvgFile(filename) {
+  return path.parse(filename).ext === '.svg';
+}
 
 function discover() {
-  const svgIcons = glob.sync(path.join(ICONS_DIRECTORY, '*.svg'))
-    .filter(file => file.indexOf('.svg') !== -1)
-    .map(filename => path.parse(filename).name);
+  const svgIcons = glob.sync(paths.content.icons.sourceFiles)
+    .filter(isSvgFile)
+    .map(filename => {
+      const parsedPath = path.parse(filename);
+      let category = parsedPath.dir.replace(`${paths.content.icons.sourceDirectory}`, '').replace('/', '');
+
+      return {
+        name: path.parse(filename).name,
+        category,
+      };
+    });
 
   const iconFontIcons = glob.sync(paths.content.iconFont.sourceFiles)
-    .filter(file => file.indexOf('.svg') !== -1)
+    .filter(isSvgFile)
     .map(filename => path.parse(filename).name);
 
   return {
-    svg: svgIcons,
+    svg: groupBy(svgIcons, 'category'),
     iconFont: iconFontIcons,
   }
 }
