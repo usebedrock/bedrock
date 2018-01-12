ea855df47c64ccbfade577616bb421bdb54a907d
Thomas Tuts <thomas@thomastuts.com>
2018-01-12 15:40:16 +0100

    Add support for using JS decorators

--- content/js/index.js
+++ content/js/index.js
@@ -1,1 +1,25 @@
+import { NgModule } from '@angular/core';
+
 require('../../core/js/index');
+import bar from './foo';
+
+console.log(bar);
+
+class Greeter {
+  greet(name) {
+    return 'Hello ' + name + '!';
+  }
+}
+
+@NgModule({
+  providers: [
+    Greeter
+  ]
+})
+class HelloWorld {
+  greeter;
+
+  constructor(greeter) {
+    this.greeter = greeter;
+  }
+}
