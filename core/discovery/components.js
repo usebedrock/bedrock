dbc014f91944be11d172d61dc17e92fc26fa6b33
Wolfr <johan.ronsse@gmail.com>
2018-04-29 14:55:36 +0200

    Allow styleguide to be unused altogether

--- core/discovery/components.js
+++ core/discovery/components.js
@@ -20,62 +20,65 @@ function discover() {
   if (fs.existsSync(paths.content.templates.patterns)) {
     console.log(chalk.white.bgRed.bold('This project still uses the old `content/templates/_patterns/` directory.\nThis directory has been renamed to `content/templates/_components/`.\nPlease rename the folder in this project.\n'));
   }
+  
+  if (config.styleguide) {
+    for (const file of files) {
+      const filename = file.replace(TEMPLATES_BASE_DIRECTORY, '').replace('.pug', '');
+      const parts = filename.split(path.sep);
+      const groupId = parts[0];
+      const componentName = parts[1];
+      const category = componentGroups[groupId];
 
-  for (const file of files) {
-    const filename = file.replace(TEMPLATES_BASE_DIRECTORY, '').replace('.pug', '');
-    const parts = filename.split(path.sep);
-    const groupId = parts[0];
-    const componentName = parts[1];
-    const category = componentGroups[groupId];
+      if (!category) {
+        componentGroups[groupId] = {
+          category: {
+            id: groupId.split('-')[0],
+            humanized: config.styleguide.componentCategories[groupId.split('-')[0]]
+          },
+          group: {
+            id: groupId
+          },
+          components: []
+        };
 
-    if (!category) {
-      componentGroups[groupId] = {
-        category: {
-          id: groupId.split('-')[0],
-          humanized: config.styleguide.componentCategories[groupId.split('-')[0]]
-        },
-        group: {
-          id: groupId
-        },
-        components: []
+        try {
+          const docsPath = path.join(TEMPLATES_BASE_DIRECTORY, groupId, '_docs.md');
+          const docsContent = fs.readFileSync(docsPath, 'utf8');
+          const parsedDocs = frontMatter(docsContent);
+
+          parsedDocs.body = marked(parsedDocs.body);
+          componentGroups[groupId].docs = parsedDocs;
+        } catch (err) {
+
+        }
+      }
+
+      const componentData = {
+        filename,
+        name: componentName,
       };
 
+
       try {
-        const docsPath = path.join(TEMPLATES_BASE_DIRECTORY, groupId, '_docs.md');
-        const docsContent = fs.readFileSync(docsPath, 'utf8');
-        const parsedDocs = frontMatter(docsContent);
+        const componentDocsPath = path.join(TEMPLATES_BASE_DIRECTORY, groupId, componentName + '.md');
+        const componentDocsContent = fs.readFileSync(componentDocsPath, 'utf8');
+        const parsedDocs = frontMatter(componentDocsContent);
 
         parsedDocs.body = marked(parsedDocs.body);
-        componentGroups[groupId].docs = parsedDocs;
+        componentData.docs = parsedDocs;
       } catch (err) {
 
       }
+
+      componentGroups[groupId].components.push(componentData);
     }
 
-    const componentData = {
-      filename,
-      name: componentName,
+    return {
+      byGroup: componentGroups,
+      byCategory: _.groupBy(componentGroups, (component) => component.category.humanized || 'No category')
     };
-
-
-    try {
-      const componentDocsPath = path.join(TEMPLATES_BASE_DIRECTORY, groupId, componentName + '.md');
-      const componentDocsContent = fs.readFileSync(componentDocsPath, 'utf8');
-      const parsedDocs = frontMatter(componentDocsContent);
-
-      parsedDocs.body = marked(parsedDocs.body);
-      componentData.docs = parsedDocs;
-    } catch (err) {
-
-    }
-
-    componentGroups[groupId].components.push(componentData);
   }
 
-  return {
-    byGroup: componentGroups,
-    byCategory: _.groupBy(componentGroups, (component) => component.category.humanized || 'No category')
-  };
 }
 
 module.exports = {
