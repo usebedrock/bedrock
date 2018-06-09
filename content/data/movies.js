bcb7e2745e6b2c0dd59bbf225f49c7e7794c8b00
Wolfr <johan.ronsse@gmail.com>
2018-06-10 01:49:42 +0200

    Fixes to material design base
    Signed-off-by: Wolfr <johan.ronsse@gmail.com>

---
+++ content/data/movies.js
@@ -0,0 +1,48 @@
+'use strict';
+
+let movies = [
+  {
+    "name": "Never Let Me Go",
+    "director": "To be filled in",
+    "rating": "To be filled in",
+    "releaseDate": "To be filled in"
+  },
+  {
+    "name": "The Game",
+    "director": "David Fincher",
+    "rating": "7",
+    "releaseDate": "17/02/2017"
+  },
+  {
+    "name": "Tomasa",
+    "director": "To be filled in",
+    "rating": "To be filled in",
+    "releaseDate": "To be filled in"
+  },
+  {
+    "name": "Tomasa",
+    "director": "To be filled in",
+    "rating": "To be filled in",
+    "releaseDate": "To be filled in"
+  },
+  {
+    "name": "Tomasa",
+    "director": "To be filled in",
+    "rating": "To be filled in",
+    "releaseDate": "To be filled in"
+  },
+  {
+    "name": "Tomasa",
+    "director": "To be filled in",
+    "rating": "To be filled in",
+    "releaseDate": "To be filled in"
+  },
+  {
+    "name": "Tomasa",
+    "director": "To be filled in",
+    "rating": "To be filled in",
+    "releaseDate": "To be filled in"
+  }
+];
+
+module.exports = movies;
--- content/js/index.js
+++ content/js/index.js
@@ -1,1 +1,4 @@
 require('../../core/js/index');
+
+//import {MDCRipple} from '@material/ripple';
+//const ripple = new MDCRipple(document.querySelector('.mdc-button'));
