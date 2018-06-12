303d23acd49fb73b6cadc3e26947e03b9e9b259c
Wolfr <johan.ronsse@gmail.com>
2018-06-12 17:00:08 +0200

    Working JS imp for Ripple+Checkbox
    Signed-off-by: Wolfr <johan.ronsse@gmail.com>

--- content/js/index.js
+++ content/js/index.js
@@ -1,6 +1,9 @@
 require('../../core/js/index');
 
+// Ripple
+import { MDCRipple } from '@material/ripple/dist/mdc.ripple';
+const ripple = new MDCRipple(document.querySelector('.mdc-button'))
 
-// Global code for buttons
-import {MDCRipple} from '@material/ripple';
-const ripple = new MDCRipple(document.querySelector('.mdc-button'));
+// Checkbox
+import {MDCCheckbox} from '@material/checkbox/dist/mdc.checkbox';
+const checkbox = new MDCCheckbox(document.querySelector('.mdc-checkbox'))
