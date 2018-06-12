00ddcd7b90765a7fa53c707e1ff69b7358258e82
Wolfr <johan.ronsse@gmail.com>
2018-06-12 17:12:35 +0200

    Init ripple for buttons
    Signed-off-by: Wolfr <johan.ronsse@gmail.com>

--- content/js/index.js
+++ content/js/index.js
@@ -1,9 +1,11 @@
 require('../../core/js/index');
 
-// Ripple
-import { MDCRipple } from '@material/ripple/dist/mdc.ripple';
-const ripple = new MDCRipple(document.querySelector('.mdc-button'))
+// Init ripple for buttons
 
-// Checkbox
-import {MDCCheckbox} from '@material/checkbox/dist/mdc.checkbox';
-const checkbox = new MDCCheckbox(document.querySelector('.mdc-checkbox'))
+import { MDCRipple } from '@material/ripple/dist/mdc.ripple';
+
+const buttons = document.querySelectorAll('.mdc-button');
+
+for (var i = 0; i < buttons.length; i++) {
+  new MDCRipple(buttons[i]);
+}
