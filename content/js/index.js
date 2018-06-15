29abdc1670c1813c61d8bd0e1ea22b9a187408b1
Wolfr <johan.ronsse@gmail.com>
2018-06-15 11:42:26 +0200

    Remove file inputs JS/Improve comments
    Signed-off-by: Wolfr <johan.ronsse@gmail.com>

--- content/js/index.js
+++ content/js/index.js
@@ -4,19 +4,17 @@ window.$ = $;
 
 // Bootstrap bundle
 require('bootstrap/dist/js/bootstrap.bundle');
+// Image holders
 require('holderjs');
 
 $(function() {
 
   // Custom javascript
-  require('./file-inputs');
   require('./tooltips');
   require('./popovers');
 
 });
 
-// Require JS to render prototype,
-// styleguide and navigation.
+// Require JS to render prototype, styleguide and navigation.
 // Remove this line when going to production.
-//
 require('../../core/js/index');
