39ae53ee2ea4e62044dfd1c591855155b85bd1b2
Wolfr <johan.ronsse@gmail.com>
2018-06-15 10:32:48 +0200

    Fix #225
    Signed-off-by: Wolfr <johan.ronsse@gmail.com>

--- core/discovery/components.js
+++ core/discovery/components.js
@@ -12,6 +12,26 @@ const paths = require('../paths');
 
 const TEMPLATES_BASE_DIRECTORY = paths.content.templates.components;
 
+function autoHumanizedName(filename) {
+  let fileNameParts = filename.split("-");
+  let humanizedNameParts = [];
+  let humanizedName = '';
+  fileNameParts.forEach((fileNamePart) => {
+    // If is not a number
+    if(!(/^\d+$/.test(fileNamePart))) {
+      // If is not a component category
+      if(Object.keys(config.styleguide.componentCategories).indexOf(fileNamePart) === -1) {
+        humanizedNameParts.push(fileNamePart);
+      }
+    }
+  })
+
+  // Final name 
+  humanizedName = humanizedNameParts.join(" ");
+  // Capitalize first letter
+  return humanizedName.substr(0,1).toUpperCase() + humanizedName.substr(1);
+}
+
 function discover() {
   const files = glob.sync(path.join(TEMPLATES_BASE_DIRECTORY, '**/*.pug')).map(file => file.replace(/\//g, path.sep));
   let componentGroups = {};
@@ -28,6 +48,7 @@ function discover() {
   for (const file of files) {
     const filename = file.replace(TEMPLATES_BASE_DIRECTORY, '').replace('.pug', '');
     const parts = filename.split(path.sep);
+
     const groupId = parts[0];
     const componentName = parts[1];
     const category = componentGroups[groupId];
@@ -41,7 +62,13 @@ function discover() {
         group: {
           id: groupId
         },
-        components: []
+        components: [],
+        docs: {
+          attributes: {
+            // Define an auto title, will be overwritten if _docs.md exists
+            title: autoHumanizedName(groupId)
+          }
+        }
       };
 
       try {
@@ -59,6 +86,12 @@ function discover() {
     const componentData = {
       filename,
       name: componentName,
+      docs: {
+        attributes: {
+          // Define an auto component title, will be overwritten if {component-name}.md exists
+          title: autoHumanizedName(componentName)
+        }
+      }
     };
 
 
