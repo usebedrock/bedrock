517df9b96aaa5e7cbed51217732af82fa45e2787
Benoit Bosschert <benoit.bosschaert@hotmail.com>
2018-06-15 13:09:58 +0200

    Tested mulitple examples + addes dependencies from @material

--- content/js/index.js
+++ content/js/index.js
@@ -2,11 +2,16 @@ require('../../core/js/index');
 
 // All @matial imports
 
-import { MDCRipple } from '@material/ripple/dist/mdc.ripple';
+import {MDCRipple} from '@material/ripple/dist/mdc.ripple';
+import {MDCChipSet} from '@material/chips/dist/mdc.chips';
+import {MDCFormField} from '@material/form-field/dist/mdc.formField';
+import {MDCCheckbox} from '@material/checkbox/dist/mdc.checkbox';
 
 // Element variables
 
 const buttons = document.querySelectorAll('.mdc-button');
+const chips = document.querySelectorAll('.mdc-chip-set');
+const checkboxes = document.querySelectorAll('.mdc-checkbox');
 
 function init() {
   // Check if there are buttons on page
@@ -15,19 +20,38 @@ function init() {
   //      handleMDCButton();
   //    }
 
-  if (!buttons) {
-    console.log('There are no buttons on this page');
-  } else {
-    handleMDCButton();
-  }
+  handleMDCButton(buttons);
+  handleMDCChips(chips);
+  handleMDCCheckboxes(checkboxes);
 }
 
 // General MDC Button function
 // Add all functions related to MDC Button page
 
-function handleMDCButton() {
-  handleRipple(buttons);
+function handleMDCButton(buttonsEl) {
+  handleRipple(buttonsEl);
 }
+
+// General MDC Chips function
+// Add all functions related to MDC Chips page
+
+function handleMDCChips(chipsEl) {
+  for (var i = 0; i < chipsEl.length; i++) {
+    new MDCChipSet(chipsEl[i]);
+  }
+}
+
+// General MDC Checkboxes function
+// Add all functions related to MDC Checkboxes page
+
+function handleMDCCheckboxes(checkboxesEl) {
+  [].forEach.call(checkboxesEl, function(checkbox) {
+    const formField = checkbox.parentElement;
+    const formFieldInstance = new MDCFormField(formField);
+    const checkboxInstance = new MDCCheckbox(checkbox);
+    formFieldInstance.input = checkboxInstance;
+  });
+};
 
 // Helper functions (reusable function)
 
