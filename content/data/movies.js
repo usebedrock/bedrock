dd2f8b3048307d3637ba6aec9ac722d92a085619
Wolfr <johan.ronsse@gmail.com>
2018-06-18 16:28:26 +0200

    Add top app bar component - improve form layout
    Signed-off-by: Wolfr <johan.ronsse@gmail.com>

--- content/data/movies.js
+++ content/data/movies.js
@@ -2,44 +2,44 @@
 
 let movies = [
   {
-    "name": "Never Let Me Go",
-    "director": "To be filled in",
+    "name": "Three Billboards outside Ebbing, Missouri",
+    "director": "Martin McDonagh",
+    "rating": "8.3",
+    "releaseDate": "January 2018"
+  },
+  {
+    "name": "La Vie D’Adele",
+    "director": "Luc Darddenne & Jean-Pierre Dardenne",
+    "rating": "7.8",
+    "releaseDate": "April 2017"
+  },
+  {
+    "name": "I, Tonya",
+    "director": "Craig Gillespie",
+    "rating": "7.6",
+    "releaseDate": "To be filled in"
+  },
+  {
+    "name": "The Avengers: Infinity War",
+    "director": "Anthony & Joe Russo",
     "rating": "To be filled in",
     "releaseDate": "To be filled in"
   },
   {
-    "name": "The Game",
-    "director": "David Fincher",
-    "rating": "7",
-    "releaseDate": "17/02/2017"
-  },
-  {
-    "name": "Tomasa",
-    "director": "To be filled in",
+    "name": "Isle of Dogs",
+    "director": "Wes Anderson",
     "rating": "To be filled in",
     "releaseDate": "To be filled in"
   },
   {
-    "name": "Tomasa",
-    "director": "To be filled in",
+    "name": "Atomic Blonde",
+    "director": "David Leitch",
     "rating": "To be filled in",
     "releaseDate": "To be filled in"
   },
   {
-    "name": "Tomasa",
-    "director": "To be filled in",
-    "rating": "To be filled in",
-    "releaseDate": "To be filled in"
-  },
-  {
-    "name": "Tomasa",
-    "director": "To be filled in",
-    "rating": "To be filled in",
-    "releaseDate": "To be filled in"
-  },
-  {
-    "name": "Tomasa",
-    "director": "To be filled in",
+    "name": "Black Panther",
+    "director": "Ryan Coogler",
     "rating": "To be filled in",
     "releaseDate": "To be filled in"
   }
