9ed532845160ff930d5edcb2afaeedbdcf4eae35
Benoit Bosschert <benoit.bosschaert@hotmail.com>
2018-06-21 17:36:43 +0200

    Update @material/icon-button

--- content/js/index.js
+++ content/js/index.js
@@ -37,7 +37,7 @@ function init() {
   handleMDCTabBarScrollers(tabbarsscrollers);
 
   handleMDCRipple(buttons);
-  // handleMDCRipple(iconbuttons);
+  handleMDCRipple(iconbuttons);
 }
 
 // General MDC Chips function
