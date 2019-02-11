324949bc74b898532099ad89690c51f409f0696b
Wolfr <johan.ronsse@gmail.com>
2019-02-11 21:55:10 +0100

    Implement styleguide search: category hide
    Signed-off-by: Wolfr <johan.ronsse@gmail.com>

--- core/js/styleguide-search.js
+++ core/js/styleguide-search.js
@@ -4,13 +4,25 @@ $('#styleguideSearch').on('input', function(e) {
 
   var inputVal = $(this).val().toUpperCase();
 
-  $('.br-styleguide-nav-holder li a').each(function() {
-    var txtValue = $(this).text().toUpperCase();
-    if (txtValue.indexOf(inputVal) > -1) {
-      $(this).parent().show();
-    } else {
-      $(this).parent().hide();
+  $('.br-docs-category').each(function() {
+
+    $(this).find('.br-docs-category-list-wrapper li a').each(function() {
+
+        var txtValue = $(this).text().toUpperCase();
+        if (txtValue.indexOf(inputVal) > -1) {
+          $(this).parent().show();
+        } else {
+          $(this).parent().hide();
+        }
+
+    });
+
+    $(this).show();
+    if( $(this).find('li:hidden').length == $(this).find('li').length ) {
+        $(this).hide();
     }
+
   });
 
 });
+
