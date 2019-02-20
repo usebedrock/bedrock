fbfadb7593f1235d14bd891271f8b6b57b1a0483
Wolfr <johan.ronsse@gmail.com>
2019-02-20 19:16:37 +0100

    Upgrade Bedrock
    Signed-off-by: Wolfr <johan.ronsse@gmail.com>

---
+++ core/js/styleguide-search.js
@@ -0,0 +1,28 @@
+const $ = require('jquery');
+
+$('#styleguideSearch').on('input', function(e) {
+
+  var inputVal = $(this).val().toUpperCase();
+
+  $('.br-docs-category').each(function() {
+
+    $(this).find('.br-docs-category-list-wrapper li a').each(function() {
+
+        var txtValue = $(this).text().toUpperCase();
+        if (txtValue.indexOf(inputVal) > -1) {
+          $(this).parent().show();
+        } else {
+          $(this).parent().hide();
+        }
+
+    });
+
+    $(this).show();
+    if( $(this).find('li:hidden').length == $(this).find('li').length ) {
+        $(this).hide();
+    }
+
+  });
+
+});
+
