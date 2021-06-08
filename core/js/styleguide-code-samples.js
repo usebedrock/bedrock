5adfe791b0414b7a2fed5ee5ed0340c432fa7816
Johan Ronsse <johan.ronsse@gmail.com>
2021-06-08 19:52:53 +0200

    Swap codemirror to prism (start)

--- core/js/styleguide-code-samples.js
+++ core/js/styleguide-code-samples.js
@@ -1,75 +1,62 @@
 import $ from 'jquery';
 import Clipboard from 'clipboard';
-import Codemirror from 'codemirror';
-import 'codemirror/mode/pug/pug';
-import 'codemirror/mode/xml/xml';
+//import Codemirror from 'codemirror';
+//import 'codemirror/mode/pug/pug';
+//import 'codemirror/mode/xml/xml';
 
+import Prism from 'prismjs';
 import config from '../discovery/config';
 
-const $codeHolders = $('.br-sample-code');
-const $codeBlocks = $('.br-sample-markup');
+const $codeHolders = document.querySelectorAll('.br-sample-code');
+const $codeBlocks = document.querySelectorAll('.br-sample-markup');
 
 if (config.styleguide) {
 
   /* Init code blocks
      ========================================================================== */
 
-  $codeBlocks.each(function () {
+  console.log($codeBlocks);
 
-    if ($(this).hasClass('br-sample-markup-html')) {
-      var mode = 'xml'
-    } else {
-      var mode = 'pug'
-    }
-
-    const editorOptions = {
-      value: $(this).text(),
-      readOnly: true,
-      mode: mode
-    };
-
-    $(this).empty();
-    $(this).editor = Codemirror(this, editorOptions);
-  });
+  $codeBlocks.forEach(item => Prism.highlight(item.innerHTML, Prism.languages.xml, 'xml')  )
 
   /* Button logic: be able to show both Pug and HTML at the same time
      ========================================================================== */
 
-  $codeHolders.hide();
+  //$codeHolders.hide();
 
-  $('.br-sample-show-code-btn-html').on('click', function(e) {
-    if ($(this).parents('.br-sample').find('.br-sample-code-html').is(':visible')) {
-        $(this).parents('.br-sample').find('.br-sample-code-html').hide();
-    } else {
-      $(this).parents('.br-sample').find('.br-sample-code-html').show();
-    }
-  });
-
-  $('.br-sample-show-code-btn-pug').on('click', function(e) {
-    if ($(this).parents('.br-sample').find('.br-sample-code-pug').is(':visible')) {
-        $(this).parents('.br-sample').find('.br-sample-code-pug').hide();
-    } else {
-      $(this).parents('.br-sample').find('.br-sample-code-pug').show();
-    }
-  });
+  // $('.br-sample-show-code-btn-html').on('click', function(e) {
+  //   if ($(this).parents('.br-sample').find('.br-sample-code-html').is(':visible')) {
+  //       $(this).parents('.br-sample').find('.br-sample-code-html').hide();
+  //   } else {
+  //     $(this).parents('.br-sample').find('.br-sample-code-html').show();
+  //   }
+  // });
+  //
+  // $('.br-sample-show-code-btn-pug').on('click', function(e) {
+  //   if ($(this).parents('.br-sample').find('.br-sample-code-pug').is(':visible')) {
+  //       $(this).parents('.br-sample').find('.br-sample-code-pug').hide();
+  //   } else {
+  //     $(this).parents('.br-sample').find('.br-sample-code-pug').show();
+  //   }
+  // });
 
   /* Save text to clipboard
      ========================================================================== */
 
-  const clipboard = new Clipboard('.br-sample-copy-code-btn', {
-    text: function (trigger) {
-      const originalButtonText = $(trigger).text();
-
-      $(trigger).prop('disabled', true);
-      $(trigger).text('Copied!');
-
-      setTimeout(function () {
-        $(trigger).text(originalButtonText);
-        $(trigger).prop('disabled', false);
-      }, 1500);
-
-      return $(trigger).siblings('.br-sample-markup').find('.CodeMirror').get(0).CodeMirror.getValue();
-    }
-  });
+  //const clipboard = new Clipboard('.br-sample-copy-code-btn', {
+  //  text: function (trigger) {
+  //    const originalButtonText = $(trigger).text();
+  //
+  //    $(trigger).prop('disabled', true);
+  //    $(trigger).text('Copied!');
+  //
+  //    setTimeout(function () {
+  //      $(trigger).text(originalButtonText);
+  //      $(trigger).prop('disabled', false);
+  //    }, 1500);
+  //
+  //    return $(trigger).siblings('.br-sample-markup').find('.CodeMirror').get(0).CodeMirror.getValue();
+  //  }
+  //});
 
 }
