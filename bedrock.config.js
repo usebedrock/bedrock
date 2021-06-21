6fe172839c0f99c4e03e50b2e1cad904f3c0dec2
Johan Ronsse <johan.ronsse@gmail.com>
2021-06-21 18:18:05 +0200

    Empty out Bedrock config

--- bedrock.config.js
+++ bedrock.config.js
@@ -1,16 +1,9 @@
 /**
  * Bedrock configuration
- * Extends the default config object in core/discovery/config.js
+ * Extends the default config object in `core/discovery/config.js`
  * For docs, see https://bedrockapp.org/documentation/configuration/
 */
 
 module.exports = {
-  js: {
-    minify: true
-  },
-  css: {
-    purge: true,
-    compiler: 'postcss',
-    minify: true
-  }
+  // Nothing specified
 };
