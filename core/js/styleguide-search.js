d9ff27c076e67f3253e637d5591689e50400ff9c
xsthetic <86376057+xsthetic@users.noreply.github.com>
2021-10-16 21:48:09 +0300

    Rewrote styleguide-search to vanilla JS

--- core/js/styleguide-search.js
+++ core/js/styleguide-search.js
@@ -1,28 +1,27 @@
-import $ from 'jquery';
+document
+  .getElementById("styleguideSearch")
+  .addEventListener("input", function (e) {
+    const inputVal = e.target.value.toUpperCase();
+    document
+      .getElementsByClassName("br-docs-category")
+      .forEach(function (docItem) {
+        docItem
+          .querySelectorAll(".br-docs-category-list-wrapper li a")
+          .each(function (el) {
+            const txtValue = el.textContent.toUpperCase();
+            if (txtValue.indexOf(inputVal) > -1) {
+              docItem.style.display = "";
+            } else {
+              docItem.style.display = "none";
+            }
+          });
 
-$('#styleguideSearch').on('input', function(e) {
-
-  var inputVal = $(this).val().toUpperCase();
-
-  $('.br-docs-category').each(function() {
-
-    $(this).find('.br-docs-category-list-wrapper li a').each(function() {
-
-        var txtValue = $(this).text().toUpperCase();
-        if (txtValue.indexOf(inputVal) > -1) {
-          $(this).parent().show();
-        } else {
-          $(this).parent().hide();
+        docItem.style.display = "";
+        if (
+          docItem.querySelectorAll("li:hidden").length ==
+          docItem.getElementsByTagName('li').length
+        ) {
+          docItem.style.display = "none";
         }
-
-    });
-
-    $(this).show();
-    if( $(this).find('li:hidden').length == $(this).find('li').length ) {
-        $(this).hide();
-    }
-
+      });
   });
-
-});
-
