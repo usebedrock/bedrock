222cab9257496403bc403769bb4886b66a4bb940
Johan Ronsse <johan.ronsse@gmail.com>
2021-11-16 17:06:25 +0100

    Rewrite remember scroll as vanilla JS

--- core/js/index.js
+++ core/js/index.js
@@ -4,10 +4,4 @@ import './prototype-nav';
 import './styleguide-code-samples';
 import './styleguide-typography';
 import './styleguide-search';
-import rememberScroll from './styleguide-remember-scroll';
-
-window.$ = $;
-
-rememberScroll('#__prototype-nav');
-rememberScroll('.br-styleguide-content');
-rememberScroll('.br-styleguide-navigation-holder');
+import './styleguide-remember-scroll';
